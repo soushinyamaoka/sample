788be515732867d3b72c15049a4d73e8adb7fef3
soushinyamaoka <s.yamaoka89@gmail.com>
2018-03-25 22:08:02 +0900

    commit

--- app/src/main/java/com/example/soushinyamaoka/sample/MainActivity.java
+++ app/src/main/java/com/example/soushinyamaoka/sample/MainActivity.java
@@ -1,138 +1,93 @@
 package com.example.soushinyamaoka.sample;
 
 import android.app.Activity;
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by syama on 2018/02/03.
  */
 
-public class MainActivity extends Activity{
-
-    private final static String DB_NAME = "test.db";//DB名
-    private final static String DB_TABLE = "test";//テーブル名
-    private final static int    DB_VERSION = 1;   //バージョン
+public class MainActivity extends Activity {
 
     private EditText editText;
     private Button editButton;
-    private Button showButton;
-    public TextView textview;
     public ListView listView;
-
-    private SQLiteDatabase db;    //データベースオブジェクト
+    DBAdapter dbAdapter = new DBAdapter(this);
+    DialogFragment newFragment = new SampleDialogFragment();
 
     //アクティビティ起動時に呼ばれる
     @Override
-    public void onCreate(Bundle bundle){
+    public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_main);
-
         editText = findViewById(R.id.editText);
         editButton = findViewById(R.id.editButton);
-        showButton = findViewById(R.id.showButton);
-        textview = findViewById(R.id.textView);
         listView = findViewById(R.id.listView);
 
-        //データベースオブジェクトの取得
-        DBHelper dbHelper = new DBHelper(this);
-        db = dbHelper.getWritableDatabase();
+        showlist();
 
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try{
+                try {
                     String str = editText.getText().toString();//書き込まれた内容(getText)をstrに格納
-                    writeDB(str);//writeDBメソッドを呼び出し、strを引数として渡す
-                }catch(Exception e){//エラーの場合
-                    textview.setText("書き込み失敗しました");
-                }
-           }
-        });
-
-        showButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try{
-                    listAdapter();
-                }catch(Exception e){
-                    textview.setText("書き込み失敗しました");
+                    if(str.equals("")){
+                        newFragment.show(getFragmentManager(), "test1");
+                    }
+                    else {
+                        dbAdapter.openDB();
+                        dbAdapter.writeDB(str);//writeDBメソッドを呼び出し、strを引数として渡す
+                        dbAdapter.readDB();
+                        showlist();
+                    }
+                } catch (Exception e) {//エラーの場合
+                    Log.e(TAG, "SQLExcepption:" + e.toString());
                 }
             }
         });
-    }
 
-    //データベースへの書き込み
-    private void writeDB(String info) throws Exception{
-        ContentValues values = new ContentValues();//値を格納するためのvaluesを宣言
-        values.put("id", "0");
-        values.put("info", info);
-        int colNum = db.update(DB_TABLE, values, null, null);
-        if(colNum == 0)db.insert(DB_TABLE, "", values);
-        //上記はINSERT INTO DB_TABLE (id, 0) VALUES("info", info);と同じ処理をしている
-    }
+        listView.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
 
-    //データベースからの読み込み
-    private String[] readDB() throws Exception{
-        String[] cols = {"info"};
-        Cursor c = db.query(DB_TABLE, cols,//query()メソッドを使用してデータ検索をする
-                "id='0'", null, null, null,null);
-        //query()の第一引数にはテーブル名、第二引数には取得対象のカラム名を配列の形で指定する。
-        //第三引数以降は取得条件(like演算子など)やgroup by句、order by句を指定する。今回はorder by句のみ指定して、あとはnull
-        //検索した結果はcに格納される
-        if(c.getCount() == 0)throw new Exception();//全件取得する場合は、moveToFirst()メソッドを使用
-        //ArrayList<String> str = new ArrayList<String>();
-        String[] str = new String[c.getCount()];
-        c.moveToFirst();                           //確実にcursor内のデータ参照先を先頭に持ってくる。
-        for(int i=0; i<c.getCount(); i++){
-            str[i] = c.getString(i);
-            c.moveToNext();
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent,View view,
+                                           int position, long id) {
+               dbAdapter.deleteDB(id);
+                try {
+                    dbAdapter.readDB();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return false;
+            }
+        });
+    }
+    public void showlist(){
+        ArrayList<String> lvAdapter = new ArrayList<>();
+        dbAdapter.openDB();
+        try {
+            lvAdapter = dbAdapter.readDB();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        c.close();
-        return str;
+        // Adapterの作成
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list, (List<String>) lvAdapter);
+        // ListViewにAdapterを関連付ける
+        listView.setAdapter(adapter);
     }
 
-    private void listAdapter() throws Exception {
-        String[] str = readDB();
-        ArrayAdapter<String> arrayAdapter =
-                new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, str);
 
-        listView.setAdapter(arrayAdapter);//strをeditTextで表示
-    }
-
-    //データベースヘルパーの定義
-    private static class DBHelper extends SQLiteOpenHelper {
-        //データベースヘルパーのコンストラクタ
-        public DBHelper(Context context){
-            super(context, DB_NAME, null, DB_VERSION); //DB名、テーブル名、DBバージョンを定数として保持している
-        }
-
-        //データベースの生成
-        //@Override
-        public void onCreate(SQLiteDatabase db){
-            db.execSQL("create table if not exists " +
-                    DB_TABLE + "(id text primary key,info text)");
-        }
-
-        //データベースのアップグレード
-        @Override
-        public void onUpgrade(SQLiteDatabase db,int oldVersion,
-                              int newVersion){
-            db.execSQL("drop table if exists "+DB_TABLE);
-            onCreate(db);
-        }
-    }
 }
---
+++ app/src/main/java/com/example/soushinyamaoka/sample/SampleDialogFragment.java
@@ -0,0 +1,31 @@
+package com.example.soushinyamaoka.sample;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by SoushinYamaoka on 2018/03/25.
+ */
+
+public class SampleDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+//CharSequenceとStringは互換あり
+        String empMessage = "タスクを入力してください";
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(empMessage)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //リストクリック時の処理
+                dialog.dismiss();
+            }
+        });
+        return builder.create();
+    }
+}
